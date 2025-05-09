67ffeb0754dcff926c30dd07eab626a0aa26d8dd
Dmitry Ledentsov <dmlled@yahoo.com>
2025-05-09 22:58:08 +0200

    don't fall back to repeated polling

--- mermaidlive_ssr/assets/js/app.js
+++ mermaidlive_ssr/assets/js/app.js
@@ -24,7 +24,7 @@ import topbar from "../vendor/topbar"
 
 let csrfToken = document.querySelector("meta[name='csrf-token']").getAttribute("content")
 let liveSocket = new LiveSocket("/live", Socket, {
-  longPollFallbackMs: 2500,
+  // longPollFallbackMs: 2500, // don't fall back for this demo
   params: {_csrf_token: csrfToken}
 })
 
